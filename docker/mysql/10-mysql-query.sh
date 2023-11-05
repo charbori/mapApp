3ad52d9ea993fc951efe78b94fa2d7d4ce9e24f0
jaehyeok <charbori@github.com>
2023-11-05 15:31:12 +0000

    mod base port 8080 -> 80

--- docker/mysql/10-mysql-query.sh
+++
@@ -1,7 +0,0 @@
-echo "
-    CREATE DATABASE IF NOT EXISTS testing;
-    CREATE DATABASE IF NOT EXISTS rentalApp;
-    GRANT ALL PRIVILEGES ON rentalApp.* TO '${MYSQL_USER}'@'localhost';
-    GRANT ALL PRIVILEGES ON testing.* TO '${MYSQL_USER}'@'localhost' with grant option;
-    flush privileges;
-" > /docker-entrypoint-initdb.d/mysql-server.sql
